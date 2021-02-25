987f0b874d487d5d6fcb875fa07ac38cafe4f715
Ben Num <sc19ban@leeds.ac.uk>
2021-02-25 15:17:53 +0000

    add home route and sample movie data

--- cinemawebapp/routes.py
+++ cinemawebapp/routes.py
@@ -1,1 +1,23 @@
+from flask import render_template
+from cinemawebapp import app
 
+# Sample movie data
+movies = [
+	{
+		'director': 'Michael Bay',
+		'title' : 'Explosions',
+		'description' : 'Boom'
+	},
+	{
+		'director': 'Guy Richie',
+		'title' : 'Hilarity',
+		'description' : 'A funny film'
+	}
+
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+	posts = Post.query.all()
+	return render_template('home.html', movies=movies)

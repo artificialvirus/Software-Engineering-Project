7e3e358472cf40e759f89fa042e42d7d5f4a4e6c
Alper Onder <sc19ao@leeds.ac.uk>
2021-03-01 12:48:24 +0300

    commit

--- cinemawebapp/__init__.py
+++ cinemawebapp/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+import os
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' 
 
-# db = SQLAlchemy(app)
+app.config.from_pyfile("config.py")
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 from cinemawebapp import routes

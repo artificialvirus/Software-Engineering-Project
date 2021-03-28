fa2e0536ae085d3dc84ef1fe4991970fe2d59e74
Sizwe <sc17t2m@leeds.ac.uk>
2021-03-28 23:57:14 +0100

    adding themes

--- cinemawebapp/routes.py
+++ cinemawebapp/routes.py
@@ -19,14 +19,14 @@ class Theme:
         self.field_colours = field_colours
         self.text_colours = text_colours
         self.font = font
-        self.font_size = "15px"
+        self.font_size = "20px"
         
 class DarkTheme(Theme):
     def __init__(self):
         super().__init__(
-            ("#06061D", "#13144d", "#28294C"),
-            ("#630024", "#7a002b", "#8c0031"),
-            ("#8fb869", "#b6ed89", "#ffffff"),
+            ("#00020f", "#090a2e", "#191847"),
+            ("#380012", "#5c001f", "#8c0031"),
+            ("#746da6", "#b7b2ff", "#ffffff"),
             "Calibri Body")
         
 def get_user_theme(theme_str="default"):
@@ -36,30 +36,47 @@ def get_user_theme(theme_str="default"):
 movies = [
     {
         'director': 'Michael Bay',
-        'title' : 'Explosions',
+        'title' : '20 Million Miles To Earth',
         'description' : 'Boom',
-        'id' : "f3b05c50"
+        'id' : "f3b05c50",
+        'poster' : "https://freeclassicimages.com/images/20-Million-Miles-To-Earth-07-movie-poster.jpg"
     },
     {
         'director': 'Guy Richie',
-        'title' : 'Hilarity',
+        'title' : '99 Women',
         'description' : 'A funny film',
-        'id' : "2c917f21"
+        'id' : "2c917f21",
+        'poster' : "https://freeclassicimages.com/images/99-Women-01-movie-poster.jpg"
+    },
+    {
+        'director': 'Guy Richie',
+        'title' : '20th century',
+        'description' : 'A funny film',
+        'id' : "2c917f21",
+        'poster' : "https://freeclassicimages.com/images/20th-century-1934-movie-poster.jpg"
+    },
+    {
+        'director': 'Guy Richie',
+        'title' : '13 Rue Madeleine',
+        'description' : 'A funny film',
+        'id' : "2c917f21",
+        'poster' : "https://freeclassicimages.com/images/13-rue-madeleine-1946-movie-poster.jpg"
     }
-
 ]
 new_release = [
 	{
 		'director': 'Michael Bay',
 		'title' : 'new film',
 		'description' : 'Boom',
-                'id' : "925da9f"
+                'id' : "925da9f",
+                'poster' : "https://freeclassicimages.com/images/99-Women-01-movie-poster.jpg"
 	},
 	{
 		'director': 'Guy Richie',
 		'title' : 'newer films',
 		'description' : 'A funny film',
-                'id' : "7faefe57"
+                'id' : "7faefe57",
+                'poster' : "https://freeclassicimages.com/images/99-Women-01-movie-poster.jpg"
 	}
 
 ]
@@ -75,13 +92,13 @@ def home():
 def popular():
 	# movies = sql query all movies and available sort by n of tickets sold
 	# new_releases = sql squery 8 newest movies and available
-	return render_template('popular.html',title = 'popular' ,new=new_release,movies=movies)
+	return render_template('popular.html', theme=get_user_theme(), title='popular' ,new=new_release,movies=movies)
 
 # For you page
 @app.route("/foryou")
 def foryou():
 	# based on genre?
-	return render_template('foryou.html',title = 'for you' , movies=movies)
+	return render_template('foryou.html', theme=get_user_theme(), title= 'for you' , movies=movies)
 
 # Search page
 @app.route("/search", methods=['GET','POST'])
@@ -107,10 +124,10 @@ def search():
 
     print(vars(forms))
     
-    more_movies = [m for m in movies for i in range(15)]
+    more_movies = movies * 10;
 
     # movies = sql movies matching criteria
-    return render_template('search.html', title='search', movies=more_movies, forms=forms)
+    return render_template('search.html', theme=get_user_theme(), title='search', movies=more_movies, forms=forms)
 
 #  Individual movie details page
 @app.route("/movie/<movie_id>")
@@ -132,7 +149,7 @@ def movie(movie_id):
     for i in range(100):
         if bool(random.getrandbits(1)):  
             movie_date = MovieDate()
-            movie_date.date = (datetime.today() + timedelta(days=i)).strftime("%Y-%m-%d")
+            movie_date.date = (datetime.today() + timedelta(days=i)).strftime("%m-%d")
             movie_date.screenings = []
             for j in range(7, 22, 4):
                 if bool(random.getrandbits(1)):
@@ -144,7 +161,7 @@ def movie(movie_id):
                 movie.movie_dates.append(movie_date)
 	
 
-    return render_template('movie.html', movie=movie)
+    return render_template('movie.html', theme=get_user_theme(), movie=movie)
 
 # About cinema page - not yet implemented
 # @app.route("/about")
@@ -181,7 +198,7 @@ def signup():
 
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
-    return render_template('signup.html', title='Sign Up', form=form)
+    return render_template('signup.html', theme=get_user_theme(), title='Sign Up', form=form)
 
 
 @app.route('/login', methods=['GET','POST'])
@@ -202,7 +219,7 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', theme=get_user_theme(), title='Sign In', form=form)
 
 
 @app.route('/logout')
@@ -224,7 +241,7 @@ def add_movie():
 
     	db.session.add(addMovie)
     	db.session.commit()
-    return render_template('addMovie.html', form=form)
+    return render_template('addMovie.html', theme=get_user_theme(), form=form)
 
 @app.route("/seats/<screening_id>", methods=['GET','POST'])
 def seats(screening_id):
@@ -242,23 +259,22 @@ def seats(screening_id):
         grid.append(row)
         
     
-    user_theme = get_user_theme()
     #movies = Post.query.all()
-    return render_template('seats.html', theme=user_theme, width=grid_width, height=grid_height, grid=grid)
+    return render_template('seats.html', theme=get_user_theme(), width=grid_width, height=grid_height, grid=grid)
 
 @app.route("/ticket/<id>")
 def ticket(id):
     booking = Booking.query.filter_by(id=id).first()
     if not booking:
-        return render_template('ticket_not_found.html', title='Invalid Ticket')
+        return render_template('ticket_not_found.html', theme=get_user_theme(), title='Invalid Ticket')
 
     screening = Screen.query.filter_by(id=booking.screen_id).first()
     if not screening:
-        return render_template('ticket_not_found.html', title='Invalid Ticket')
+        return render_template('ticket_not_found.html', theme=get_user_theme(), title='Invalid Ticket')
 
     movie = Movies.query.filter_by(id=screening.movie_id).first()
     if not movie:
-        return render_template('ticket_not_found.html', title='Invalid Ticket')
+        return render_template('ticket_not_found.html', theme=get_user_theme(), title='Invalid Ticket')
 
     ticket_id=booking.id
     movie_title = movie.name
@@ -268,7 +284,7 @@ def ticket(id):
     seat_number = booking.seat_number
     ticket_code = booking.ticket_code
 
-    return render_template('ticket.html', title='Your Ticket',
+    return render_template('ticket.html', theme=get_user_theme(), title='Your Ticket',
         movie_title=movie_title, screening_date=screening_date, movie_duration=movie_duration,
         screen_number=screen_number, seat_number=seat_number, ticket_code=ticket_code)
 
@@ -276,7 +292,7 @@ def ticket(id):
 def ticket_download(ticket_code):
     booking = Booking.query.filter_by(ticket_code=ticket_code).first()
     if not booking:
-        return render_template('ticket_not_found.html', title='Invalid Ticket')
+        return render_template('ticket_not_found.html', theme=get_user_theme(), title='Invalid Ticket')
 
     screening = Screen.query.filter_by(id=booking.screen_id).first()
     if not screening:
@@ -284,7 +300,7 @@ def ticket_download(ticket_code):
 
     movie = Movies.query.filter_by(id=screening.movie_id).first()
     if not movie:
-        return render_template('ticket_not_found.html', title='Invalid Ticket')
+        return render_template('ticket_not_found.html', theme=get_user_theme(), title='Invalid Ticket')
 
     ticket_id=booking.id
     movie_title = movie.name
